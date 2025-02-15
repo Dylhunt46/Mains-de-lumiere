87ebdbe39f03aa56f45a9f31bb95d38d2f846816
Laurent Houssin <dylhunt46@gmail.com>
2025-02-15 18:59:13 +0100

    Start website  structur

---
+++ calendrier.php
@@ -0,0 +1,7 @@
+<?php
+// Logique pour récupérer les disponibilités du magnétiseur
+
+// Logique pour enregistrer le rendez-vous dans une base de données ou un fichier
+
+// Envoi d'une notification au webmaster (utilisation de la fonction mail() de PHP ou d'une librairie d'envoi d'e-mails)
+?>
---
+++ script.js
@@ -0,0 +1,5 @@
+// Code pour générer le calendrier (utilisation d'une librairie comme FullCalendar ou intégration manuelle)
+
+// Gestion des interactions pour la prise de rendez-vous
+
+// Envoi d'une alerte ou d'un message au webmaster (utilisation d'une fonction d'envoi d'e-mail en AJAX ou d'une API de notification)
